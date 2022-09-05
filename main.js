5ff20ba0e51fcd7b87a28ffde83ead6d17f12266
winnie <mwendwawinfred8@gmail.com>
2022-09-05 13:56:50 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? console.log("true")
+    : console.log("it doesn't");
+  primaryNav.toggleAttribute("data-visible");
+  
+}); 
